c75b7640b51ad96da79b58cc9fd9e0b400a9081e
Francisco Benavides <francisco.benavides@gmail.com>
2018-06-12 18:14:43 -0500

    Replacingg Bootstrap with dding Semantic UI

--- school/static/js/welcome.jsx
+++ school/static/js/welcome.jsx
@@ -3,6 +3,9 @@ import React from "react";
 
 export default class Welcome extends React.Component {
   render () {
-    return <h1>Welcome to React</h1>;
+    return (
+      <div class="ui message">
+          <h1 lass="ui header">Welcome to React</h1>
+      </div>);
   }
 }
