cb580d49d6f684b5413875f6e9bca0fdf2d2941a
Francisco Benavides <francisco.benavides@gmail.com>
2018-06-13 15:59:26 -0500

    Semantic UI with responsive mobile menu

--- school/static/js/welcome.jsx
+++ school/static/js/welcome.jsx
@@ -4,8 +4,8 @@ import React from "react";
 export default class Welcome extends React.Component {
   render () {
     return (
-      <div class="ui message">
-          <h1 lass="ui header">Welcome to React</h1>
+      <div className="ui message">
+          <h1 className="ui header">Welcome to React</h1>
       </div>);
   }
 }
