97db3a891cb9cd67860371f3ad6bae991ae972af
Francisco Benavides <francisco.benavides@gmail.com>
2018-06-14 19:04:27 -0500

    Content improvements, added the about page

--- school/urls.py
+++ school/urls.py
@@ -21,6 +21,7 @@ from django.urls import include
 urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
     re_path(r'^accounts/', include('django.contrib.auth.urls')),
+    re_path(r'^about$', TemplateView.as_view(template_name='about.html'), name='about'),
     re_path(r'^', include('registration.urls')),
     re_path(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
 ]
